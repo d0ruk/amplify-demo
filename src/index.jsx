683b119e23dc8de23382df53e947543801c2d313
Doruk Kutlu <dorukk@gmail.com>
2019-02-03 23:06:15 +0300

    remove injectGlobal | import per @amplify/package

--- src/index.jsx
+++ src/index.jsx
@@ -1,68 +1,30 @@
-import Amplify from "aws-amplify"
-import React from "react"
-import { render } from "react-dom"
-import { BrowserRouter as Router } from "react-router-dom"
-import { injectGlobal } from "styled-components"
-import { normalize, lighten, complement } from "polished"
-// import { Provider, Client } from "urql"
+import "@babel/polyfill";
+import Auth from "@aws-amplify/auth";
+import Analytics from "@aws-amplify/analytics";
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import { createBreakpoints } from "~/util"
-import aws_exports from "~/aws-exports"
-import App from "~/App"
+import { createBreakpoints } from "~/util";
+import awsconfig from "~/aws-exports";
+import App from "~/App";
 
-Amplify.configure(aws_exports);
+Auth.configure(awsconfig);
+Analytics.configure(awsconfig);
 
-// const client = new Client({
-//   url: "https://api.github.com/graphql"
-// });
 const sizes = {
-  xl: 1690, lg: 1280,
-  md: 980, sm: 736, xs: 480
+  xl: 1690,
+  lg: 1280,
+  md: 980,
+  sm: 736,
+  xs: 480
 };
-const brk = createBreakpoints(sizes);
-
-injectGlobal`
-  ${normalize()}
-  @import url("https://cdnjs.cloudflare.com/ajax/libs/devicons/1.8.0/css/devicons.min.css");
-
-  :root {
-    --primary: rgb(23, 121, 186);
-    --secondary: rgb(118, 118, 118);
-    --success: rgb(58, 219, 118);
-    --error: rgb(204, 75, 55);
-    --warning: rgb(255, 174, 0);
-    --primary-font_color: ${lighten(0.5, complement("rgb(23, 121, 186)"))};
-    --secondary-font_color: ${lighten(0.5, complement("rgb(118, 118, 118)"))};
-    --success-font_color: ${lighten(0.5, complement("rgb(58, 219, 118)"))};
-    --error-font_color: ${lighten(0.5, complement("rgb(204, 75, 55)"))};
-    --warning-font_color: ${lighten(0.5, complement("rgb(255, 174, 0)"))};
-  }
-  
-  body {
-    &:focus {
-      margin: 0;
-      padding: 0;
-      outline: 0;
-    }
-  }
-
-  html {
-    font-size: 18px;
-    text-shadow: 0 2px 0 rgba(0, 0, 0, .07);
-    ${brk.lg`font-size: 17px;`}
-    ${brk.md`font-size: 15px;`}
-    ${brk.sm`font-size: 14px;`}
-    ${brk.xs`font-size: 12px;`}
-  }
-`;
 
 render(
-  // <Provider client={client}>
   <Router>
-    <App breakpoints={brk} />
+    <App breakpoints={createBreakpoints(sizes)} />
   </Router>,
-  // </Provider>,  
   document.getElementById("root")
 );
 
-if (module.hot) { module.hot.accept(); }
+if (module.hot) module.hot.accept();
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,12 @@
-import React, { Component, Fragment } from "react"
-// import { Auth } from "aws-amplify"
-import { Connect, query } from "urql"
-import { parse, stringify } from "query-string"
-import { Portal } from "react-portal"
+import React, { Component, Fragment } from "react";
+import { Portal } from "react-portal";
 
-import { Loader, Button } from "~/components"
-
-const inspectQuery = `
-  query {
-    __schema {
-      types {
-        name
-        kind
-        description
-        fields {
-          name
-        }
-      }
-    }
-  }
-`;
+import { Loader, Button } from "~/components";
 
 export default class Home extends Component {
-
-  componentWillMount = async () => {
-    const params = parse(window.location.search);
-
-    if (params.code) {
-      console.log("there is a code!");
-    }
-  }
-  
   render() {
-    return ["primary", "secondary", "error", "warning", "success"]
-      .map(accent => [
+    return ["primary", "secondary", "error", "warning", "success"].map(accent =>
+      [
         <Button
           key={accent}
           // disabled
@@ -41,53 +14,26 @@ export default class Home extends Component {
         >
           {accent}
         </Button>,
-        <Button
-          key={accent + "-disabled"}
-          disabled
-          accent={accent}
-        >
-        disabled {accent}
+        <Button key={accent + "-disabled"} disabled accent={accent}>
+          disabled {accent}
         </Button>
-      ].concat(["bold", "bolder", "lighter", "700"].map(weight => (
-        <Button
-          key={`${weight}_${accent}`}
-          accent={accent}
-          weight={weight} /* default weight is "normal" */
-        >
-          {weight} {accent}
-        </Button>
-      ))));
-    // return (
-    //   <Connect
-    //     query={query(inspectQuery)}
-    //     render={({ loaded, data, refetch }) => {
-    //       console.dir(data);
-          
-    //       return (
-    //         <div>
-    //           {!loaded
-    //             ? this.renderLoader()
-    //             : "hello"}
-    //         </div>
-    //       );
-    //     }}
-    //   />
-    // );
-  }
-
-  handleAuth = () => {
-    const opts = {
-      client_id: "700c0603924c7fa8fdd4",
-      // redirect: "http://localhost:8080",
-      // scope: "user public_repo repo repo_deployment repo:status read:repo_hook read:org read:public_key read:gpg_key"
-    };
-    const url = `https://github.com/login/oauth/authorize?${stringify(opts)}`;
-    window.open(url);
+      ].concat(
+        ["bold", "bolder", "lighter", "700"].map(weight => (
+          <Button
+            key={`${weight}_${accent}`}
+            accent={accent}
+            weight={weight} /* default weight is "normal" */
+          >
+            {weight} {accent}
+          </Button>
+        ))
+      )
+    );
   }
 
   renderLoader = () => (
     <Portal>
       <Loader />
     </Portal>
-  )
+  );
 }
